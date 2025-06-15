057a7580edbcb43559c15edad270ffa088fc5685
JasonHongGG <p76134040@gs.ncku.edu.tw>
2025-06-15 22:42:30 +0800

    feat: pre/next command

--- Shell/Src/readLine.c
+++ Shell/Src/readLine.c
@@ -9,9 +9,58 @@
 static char prompt[PROMPT_LEN] = "> ";
 extern UART_HandleTypeDef* shell_huart;
 
+#define CMD_HISTORY_COUNT  		2
+#define CMD_HISTORY_MAX_LEN    64
+static char historyCommand[CMD_HISTORY_COUNT][CMD_HISTORY_MAX_LEN];
+static int  head = 0;    // 下一筆寫入索引
+static int  cnt = 0;     // 已存筆數 (<= SHELL_HISTORY_COUNT)
+static int  pos = -1;     // 瀏覽指標 (-1 表示未在瀏覽)
+
+
 void ReadLine_Init(UART_HandleTypeDef* huart)
 {
 	shell_huart = huart;
+	for(int i=0;i<CMD_HISTORY_COUNT;i++) historyCommand[i][0]=0;
+}
+
+void CommandHistory_Add(const char *cmd)
+{
+    if (*cmd == '\0') return;
+    // 避免重複同一筆
+    if (cnt>0) {
+        int last = (head+CMD_HISTORY_COUNT-1)%CMD_HISTORY_COUNT;
+        if (strcmp(historyCommand[last], cmd)==0) return;
+    }
+    strncpy(historyCommand[head], cmd, CMD_HISTORY_MAX_LEN-1);
+    historyCommand[head][CMD_HISTORY_MAX_LEN-1] = '\0';
+    head = (head+1) % CMD_HISTORY_COUNT;
+    if (cnt < CMD_HISTORY_COUNT) cnt++;
+    pos = -1;
+}
+
+bool CommandHistory_Prev(char *out)
+{
+    if (cnt==0) return false;
+    if (pos < 0) {
+        pos = (head+CMD_HISTORY_COUNT-1)%CMD_HISTORY_COUNT;
+    } else {
+        pos = (pos+CMD_HISTORY_COUNT-1)%CMD_HISTORY_COUNT;
+    }
+    strcpy(out, historyCommand[pos]);
+    return true;
+}
+
+bool CommandHistory_Next(char *out)
+{
+    if (cnt==0 || pos<0) return false;
+    pos = (pos+1) % CMD_HISTORY_COUNT;
+    if (pos == head) {
+        out[0] = '\0';
+        pos = -1;
+        return true;
+    }
+    strcpy(out, historyCommand[pos]);
+    return true;
 }
 
 static bool GetChar(char *out_char)
@@ -65,6 +114,7 @@ uint32_t ReadLine(char* readBuffer, int bufferSize)
 					*(pCur + 1) = '\0';
 	//				PutString("\n\r");
 	//				PutString(readBuffer);
+					CommandHistory_Add(readBuffer);
 					return (pCur - readBuffer);
 				}
 				else /* Buffer full (Overflow) */
@@ -85,6 +135,7 @@ uint32_t ReadLine(char* readBuffer, int bufferSize)
 				break;
 
 			case 0x7F: /* DEL - backspace	*/
+			case 0x08:
 				// 鼠標位置大於 promote
 				if ((outputColumnCnt > PromptLen) && (pCur > &readBuffer[0]) && (receiveCharCnt > 0x00))
 				{
@@ -95,6 +146,30 @@ uint32_t ReadLine(char* readBuffer, int bufferSize)
 					PutString("\n\r> ");
 					PutString(readBuffer);
 				}
+				break;
+
+			case '[':
+				char preCmd[CMD_HISTORY_MAX_LEN];
+				if(!CommandHistory_Prev(preCmd)) break;
+				PutString("\n\r> ");
+				PutString(preCmd);
+				strncpy(readBuffer, preCmd, sizeof(preCmd));
+				int preCmdLen = strlen(readBuffer);
+				pCur = &readBuffer[preCmdLen];
+				outputColumnCnt = PromptLen + preCmdLen;
+				receiveCharCnt = preCmdLen;
+				break;
+
+			case ']':
+				char nextCmd[CMD_HISTORY_MAX_LEN];
+				if(!CommandHistory_Next(nextCmd)) break;
+				PutString("\n\r> ");
+				PutString(nextCmd);
+				strncpy(readBuffer, nextCmd, sizeof(nextCmd));
+				int nextCmdLen = strlen(readBuffer);
+				pCur = &readBuffer[nextCmdLen];
+				outputColumnCnt = PromptLen + nextCmdLen;
+				receiveCharCnt = nextCmdLen;
 				break;
 
 			default:
